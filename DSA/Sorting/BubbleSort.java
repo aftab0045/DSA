54b3751c6f12e2211d19452ef6f1d9b2bc048a15
Aftab Attar <167393427+aftab0045@users.noreply.github.com>
2024-08-19 11:38:01 +0530

    Update BubbleSort.java

--- DSA/Sorting/BubbleSort.java
+++ DSA/Sorting/BubbleSort.java
@@ -17,6 +17,7 @@ public class BubbleSort {
             arr[i] = sc.nextInt();
         }
 
+        // Bubble Sort
         for(int i=0;i<n-1;i++){
             for(int j =0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
