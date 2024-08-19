9115f36e3e1d9f847cf2974c089e514cd00a77bc
aftab0045 <aftabattar0045@gmail.com>
2024-08-19 11:36:49 +0530

    Add

---
+++ DSA/Sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BubbleSort {
+
+    // Print the given arr
+    static void printArray(int[] arr ){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Enter the arr size");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the data in array");
+        int [] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<n-1;i++){
+            for(int j =0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        printArray(arr);
+    }
+}
