94c3586302c0358441586a53f0c6a3739981c9c5
aftab0045 <aftabattar0045@gmail.com>
2024-09-09 20:11:53 +0530

    add

---
+++ DSA/Array/sumArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class sumArray {
+
+    static int sumOfArray(int[] arr, int target){
+        int n = arr.length;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(arr[i]+arr[j] == target){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the size of array");
+        int n = sc.nextInt();
+        System.out.println("Enter "+n+" Data in Array");
+        int [] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target");
+        int target = sc.nextInt();
+
+        System.out.println(sumOfArray(arr, target));
+    }
+}
