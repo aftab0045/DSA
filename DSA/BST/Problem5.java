605b9036ed28e7ecbd04ab18cdc9423508940ec2
aftab0045 <aftabattar0045@gmail.com>
2024-09-20 23:08:35 +0530

    add

---
+++ DSA/BST/Problem5.java
@@ -0,0 +1,45 @@
+public class Problem5 {
+
+    static int findIDX(int[] arr, int target){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                ++start;
+                --end;
+            }
+            else if(arr[mid] <= arr[end]){
+                if(target > arr[mid] && target <= arr[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+                
+            } 
+            else{
+                if(target >= arr[start] && target < arr[mid]){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,2,2,3,1,1};
+        int target = 3;
+
+        System.out.println(findIDX( arr, target ));
+    }
+}
