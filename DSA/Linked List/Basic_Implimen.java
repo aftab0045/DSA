5118f8c98386bf2719337a2db988734864e7978e
aftab0045 <aftabattar0045@gmail.com>
2024-09-25 09:30:16 +0530

    add

--- DSA/Linked List/Basic_Implimen.java
+++ DSA/Linked List/Basic_Implimen.java
@@ -19,7 +19,24 @@ public class Basic_Implimen {
         b.next = c;
         c.next = d;
 
-        System.out.println(a.data);       // print data at node a
-        System.out.println(a.next.data);  // print data at next node without need of actual data 
+        // System.out.println(a.data);       // print data at node a
+        // System.out.println(b.data);       // print data at node b
+        // System.out.println(a.next.data);  // print data at next node without need of actual data
+        
+        // print the whole linkedList
+
+        // Node temp = a;                                  // use only when you know the number of nodes
+        // for(int i = 1; i <= 4; i++){
+        //     System.err.print(temp.data+" ");
+        //     temp = temp.next;
+        // }
+
+        // if you dont know the number of nodes use while loop
+
+        Node temp = a;
+        while (temp != null) {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
     } 
 }
