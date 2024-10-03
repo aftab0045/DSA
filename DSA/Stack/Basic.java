3e43c3c168718987889d20bb4ba28f9880d8f5a8
aftab0045 <aftabattar0045@gmail.com>
2024-10-03 10:52:30 +0530

    add

--- DSA/Stack/Basic.java
+++ DSA/Stack/Basic.java
@@ -1,6 +1,34 @@
-
+import java.util.*;
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("Stack");
+        Stack<Integer> st = new Stack<>();
+        System.out.println("Size is : "+st.size());
+        // push operation
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        System.out.println(st);
+        System.out.println("Size is : "+st.size());
+        st.pop();
+        // peek gives you head of stack
+        System.out.println("Current Head Is : "+st.peek());
+
+        st.push(4);
+        st.push(5);
+        st.push(6);
+        System.out.println(st);
+        // we can do multiple pop in loop
+        while(st.size()>2){
+            st.pop();
+        }
+
+        System.out.println(st);
+
+        while(st.size()<5){
+            st.push(3);
+        }
+
+        System.out.println(st);
     }
 }
