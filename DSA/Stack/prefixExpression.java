41648cc1d5d939936b7a5dcc2991a18fb444666d
aftab0045 <aftabattar0045@gmail.com>
2024-10-05 20:58:08 +0530

    add

---
+++ DSA/Stack/prefixExpression.java
@@ -0,0 +1,83 @@
+import java.util.*;
+public class prefixExpression {
+    public static void main(String[] args) {
+        String infix = "9-(5+3)*4/6";
+
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for(int i = 0; i < infix.length(); i++){
+            char ch = infix.charAt(i);
+            int ascii = (int)ch;
+
+            if(ascii>= 48 && ascii<= 57){
+                String s = "" + ch;
+                val.push(s);
+            }
+
+            else if(op.size() == 0 || ch == '(' || op.peek() == '('){
+                op.push(ch);
+            }
+
+            else if(ch == ')'){
+                while (op.peek() != '(') {
+                    
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char c = op.pop();
+
+                    String s = c + v1 + v2;
+
+                    val.push(s);
+                    
+                }
+                op.pop();
+            }
+
+            else{
+                if(ch == '-' || ch == '+'){
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char c = op.pop();
+
+                    String s = c + v1 + v2;
+
+                    val.push(s);
+                    op.push(ch);
+                }
+
+                if(ch == '*' || ch == '/'){
+                    if(op.peek() == '*' || op.peek() == '/'){
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char c = op.pop();
+
+                    String s = c + v1 + v2;
+
+                    val.push(s);
+                    op.push(ch);
+                    }
+
+                    else {
+                        op.push(ch);
+                    }
+                }
+            }
+        }
+
+
+        while (val.size() > 1) {
+            String v2 = val.pop();
+                    String v1 = val.pop();
+                    char c = op.pop();
+
+                    String s = c + v1 + v2;
+
+                    val.push(s);
+        }
+
+
+        System.out.println(val.peek());
+        
+    }
+}
