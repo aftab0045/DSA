991788c132ac66abcfe46d5788eac395da111e4d
aftab0045 <aftabattar0045@gmail.com>
2024-10-08 19:36:59 +0530

    add

--- DSA/Queue/basics.java
+++ DSA/Queue/basics.java
@@ -1,5 +1,25 @@
+// Basics of queue
+
+import java.util.*;
 public class basics {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println(q);          // Display
+
+        q.remove();                     // Remove top element from queue 
+        System.out.println(q);
+
+        System.out.println(q.element());           // Display Top element
+
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println(" Size Is : "+q.size());
+        
     }
 }
