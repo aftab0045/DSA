2bfa428e72ee7ceacefc44c34ef086141060b569
aftab0045 <aftabattar0045@gmail.com>
2024-10-09 10:15:06 +0530

    add

--- DSA/Queue/problem1.java
+++ DSA/Queue/problem1.java
@@ -1,5 +1,29 @@
+// Reverse elements in Queue
+
+import java.util.*;
 public class problem1{
     public static void main(String[] args) {
-        System.out.println("Hwllo");
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println("Orignal QUeue");
+        System.out.println(q);
+
+        Stack<Integer> st = new Stack<>();
+
+        while (q.size() > 0) {
+            st.push(q.remove());
+        }
+
+        while (st.size() > 0) {
+            q.add(st.pop());
+        }
+
+        System.out.println("After Reverse : ");
+        System.out.println(q);
     }
 }
